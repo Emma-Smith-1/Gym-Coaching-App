72c060820b4e5edc9a84bc0057e1fdea27c07b0e
Emma Smith <emmasmith14682@gmail.com>
2024-03-10 13:35:06 +0000

    Created posts database

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home/', function () {
+    return "This is the homepage";
+});
+
+Route::redirect('/homepage/', '/home/');
+
+Route::get('/home/{name}/', function ($name) {
+    return "This is $name's page";
+});
