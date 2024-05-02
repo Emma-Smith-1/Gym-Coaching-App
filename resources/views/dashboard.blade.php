f15bce6fa8039f9288d2a370602e24e690c1f248
Emma Smith <emmasmith14682@gmail.com>
2024-05-02 18:29:57 +0100

    Login page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,10 +1,14 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
 
+@extends('layouts.app')
+
+@section('title', 'Dashboard')
+
+@section('content')
+    <h1 name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __( ' Dashboard' ) }}
+        </h2>
+    </h1>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
@@ -14,4 +18,4 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+@endsection
