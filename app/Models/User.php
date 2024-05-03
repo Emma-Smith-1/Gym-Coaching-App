c5bf5eb7848818f52b5dcd769a441a929accdd47
Emma Smith <emmasmith14682@gmail.com>
2024-05-03 05:38:55 +0100

    my comments working

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,6 +31,11 @@ class User extends Authenticatable
     {
         return $this->hasMany(Post::class);
     }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
     
     /**
      * The attributes that are mass assignable.
