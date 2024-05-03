90dd718111292537f41611f7403cdc4b17c4ee1f
Emma Smith <emmasmith14682@gmail.com>
2024-05-03 02:41:30 +0100

    fixes

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,12 +3,11 @@
 @section('title', 'Posts')
 
 @section('content')
-    <h1 name="header">
-        <h2 class="p-6 font-semibold text-xl text-gray-800 dark:text-gray-200 flex items-center justify-center">
-            {{ __('All Posts') }}
-        </h2>
+    <h1 name="header" class="p-6 font-semibold text-xl text-gray-800 dark:text-gray-200 flex items-center justify-center">
+        {{ __('All Posts') }}
     </h1>
-    <a href="{{ route('posts.create' )}}" class="flex items-center justify-center">Create Post</a>
+    <a href="{{ route('posts.create') }}" class="flex items-center justify-center mb-4">Create Post</a>
+
     <ul>
         @foreach ($posts as $post)
             <div class="py-4"> <!-- Increased padding for bigger posts -->
@@ -17,9 +16,14 @@
                         <div class="p-6 text-gray-900 dark:text-gray-100">
                             <div class="text-right text-sm text-gray-500 dark:text-gray-400 mb-2">Category: {{ $post->category }}</div>
                             <div class="text-right text-sm text-gray-500 dark:text-gray-400 mb-2">Posted by: {{ $post->coach_id }}</div>
-                            <li><a href="/posts/{{ $post->id }}" class="font-bold text-lg">{{ $post->title }}</a></li> <!-- Increased font size -->
+                            <li>
+                                <a href="/posts/{{ $post->id }}" class="font-bold text-lg">{{ $post->title }}</a>
+                                <form method="POST" action="{{ route('posts.like', ['post' => $post]) }}" class="inline">
+                                    @csrf
+                                    <button type="submit">{{ $post->likes }} Likes</button>
+                                </form>
                             <ul>
-                                <li><a href="{{ route('posts.comments.index', ['post' => $post->id]) }}" class="text-red-500">View Comments</a></li>
+                                <li><a href="{{ route('posts.comments.index', ['post' => $post->id]) }}" class="text-red-500">View Comments ({{ $post->comments }})</a></li>
                             </ul>
                         </div>
                     </div>
@@ -27,6 +31,4 @@
             </div>
         @endforeach
     </ul>
-
-
 @endsection
